2b4873fb48109c17564209d8061f05eb600998f7
D <kritantablake@gmail.com>
2021-11-28 18:57:04 +0300

    Gnome

---
+++ Gnome_sort/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Gnome_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static int[] Gnome(int[] unsortedArray)
+        {
+            var index = 1;
+            var nextIndex = index + 1;
+            while (index < unsortedArray.Length)
+            {
+                if (unsortedArray[index - 1] < unsortedArray[index])
+                {
+                    index = nextIndex;
+                    nextIndex++;
+                }
+                else
+                {
+                    Swap(ref unsortedArray[index - 1], ref unsortedArray[index]);
+                    index--;
+                    if (index == 0)
+                    {
+                        index = nextIndex;
+                        nextIndex++;
+                    }
+                }
+            }
+            return unsortedArray;
+        }
+        private static void Swap(ref int a, ref int b)
+        {
+            a ^= b;
+            b ^= a;
+            a ^= b;
+        }
+    }
+}

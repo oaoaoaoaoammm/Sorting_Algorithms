907447ce9b8b874540219e3bbe38376f31734f15
D <kritantablake@gmail.com>
2021-11-28 18:52:08 +0300

    Bucket

---
+++ Bucket_sort/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Bucket_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+		public static void Bucket(int[] data)
+		{
+			int minValue = data[0];
+			int maxValue = data[0];
+
+			for (int i = 1; i < data.Length; i++)
+			{
+				if (data[i] > maxValue)
+					maxValue = data[i];
+				if (data[i] < minValue)
+					minValue = data[i];
+			}
+
+			List<int>[] bucket = new List<int>[maxValue - minValue + 1];
+
+			for (int i = 0; i < bucket.Length; i++)
+			{
+				bucket[i] = new List<int>();
+			}
+
+			for (int i = 0; i < data.Length; i++)
+			{
+				bucket[data[i] - minValue].Add(data[i]);
+			}
+
+			int k = 0;
+			for (int i = 0; i < bucket.Length; i++)
+			{
+				if (bucket[i].Count > 0)
+				{
+					for (int j = 0; j < bucket[i].Count; j++)
+					{
+						data[k] = bucket[i][j];
+						k++;
+					}
+				}
+			}
+		}
+	}
+}
+

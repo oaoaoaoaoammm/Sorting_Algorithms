074c45a44810d2c244a7daf1317f8dd57dccb5f2
D <kritantablake@gmail.com>
2021-11-28 19:00:06 +0300

    Selection

---
+++ Selection_sort/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Selection_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void Selection(int[] arr)
+        {
+            int n = arr.Length;
+            for (int i = 0; i < n - 1; i++)
+            {
+                int min_idx = i;
+                for (int j = i + 1; j < n; j++)
+                    if (arr[j] < arr[min_idx])
+                        min_idx = j;
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+        }
+    }
+}
+

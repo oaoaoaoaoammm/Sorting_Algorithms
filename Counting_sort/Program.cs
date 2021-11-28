c5f14826a4657fb9b50055c3e83aa4f59a8ffa93
D <kritantablake@gmail.com>
2021-11-28 18:56:06 +0300

    Counting

---
+++ Counting_sort/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Counting_sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+        static void Count(char[] arr)
+        {
+            int n = arr.Length;
+            char[] output = new char[n];
+            int[] count = new int[256];
+
+            for (int i = 0; i < 256; ++i)
+                count[i] = 0;
+            for (int i = 0; i < n; ++i)
+                ++count[arr[i]];
+            for (int i = 1; i <= 255; ++i)
+                count[i] += count[i - 1];
+            for (int i = n - 1; i >= 0; i--)
+            {
+                output[count[arr[i]] - 1] = arr[i];
+                --count[arr[i]];
+            }
+            for (int i = 0; i < n; ++i)
+                arr[i] = output[i];
+        }
+    }
+}
+
